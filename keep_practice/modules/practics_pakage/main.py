1b95354f83090002fafb818aa28f61d511bde6c3
Ramesh kumar <190596619+Ramesh8dsaiml@users.noreply.github.com>
2026-02-11 16:01:28 +0530

    Add files via upload

---
+++ keep_practice/modules/practics_pakage/main.py
@@ -0,0 +1,23 @@
+from mymath.operations import add, multiply
+
+print(add(5, 3))
+print(multiply(4, 6))
+
+
+from mymath.operations import subtract
+print(subtract(100 ,54))
+
+
+from mymath.operations import add
+from mymath.advanced import square_root
+
+print(add(10, 20))
+print(square_root(25))
+
+
+
+from mymath import add, square_root
+
+print(add(5, 5))
+print(square_root(16))
+
---
+++ keep_practice/modules/practics_pakage/mymath/advanced.py
@@ -0,0 +1,7 @@
+import math
+
+def square_root(x):
+    return math.sqrt(x)
+
+def power(a, b):
+    return a ** b
